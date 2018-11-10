41b25a7f43621422575e1491cb0e20cf95a34100
Chris Woodward <cwoodward5@hotmail.com>
2018-11-10 11:12:04 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
-
+import { ConfiguratorComponent } from './configurator/configurator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfiguratorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
